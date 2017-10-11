d1a62b631047bb1475663f042072099ac390e992
therealplato <therealplato@gmail.com>
2017-10-11 21:07:05 +1300

    document planned api

--- doc.go
+++ doc.go
@@ -1,15 +1,15 @@
 package main
 
 // Doc documents behavior
-const Doc = `
-timez is a cli tool for converting time between timezones
+const Doc = `timez is a cli tool for converting time between timezones
 
-it thinks your timezone is, from highest priority to lowest priority:
+most of this is still todo
+
+ it thinks your timezone is, from highest priority to lowest priority:
 - the contents of ~/.timezrc
 - the output of date +%z
 - UTC
-
-invocation:
+- Pacific/Auckland
 
 # output current local and current UTC:
 timez 
@@ -17,11 +17,14 @@ timez
 # output current PT:
 timez PT
 
-# what is my local time in pacific time:
-timez 2017-10-11 19:05:00 in America/Los_Angeles
-timez 2017-10-11 19:05:00 to US/Pacific
-timez 2017-10-11 19:05:00 to Pacific
-timez 2017-10-11 19:05:00 to PT
+# output current eastern, local, UTC:
+timez ET @ UTC
+
+# what is this local time in pacific time:
+timez 2017-10-11 19:05:00 US/Pacific
+
+# what is this zoned time in pacific time:
+timez 2017-10-11 19:05:00 -0400 US/Pacific
 
 # what is this pacific time in my local:
 timez PT 2017-10-11 19:05:00
