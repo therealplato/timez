f8347532d5ab6d3e67b5107e782b3d5a2ec78f0f
therealplato <therealplato@gmail.com>
2017-10-12 21:15:27 +1300

    break apart parse()

--- parse.go
+++ parse.go
@@ -8,54 +8,77 @@ import (
 	"time"
 )
 
-var tsFormats = []string{
-	"2006-01-02 15:04:05",
-	"2006-01-02 15:04:05 -0700",
-	time.RFC1123,
-	time.RFC1123Z,
-	time.RFC3339,
-	time.RFC3339Nano,
-	time.RFC822,
-	time.RFC822Z,
-	time.RFC850,
-	time.ANSIC,
-}
-
-var errParsingTimestamp = errors.New("found numbers that did not match a known timestamp format")
+var (
+	tsFormats = []string{
+		"2006-01-02 15:04:05",
+		"2006-01-02 15:04:05 -0700",
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		time.ANSIC,
+	}
+	zoneRX              = regexp.MustCompile(`^[a-zA-Z+\-]`)
+	fragRX              = regexp.MustCompile("^[0-9]")
+	errParsingTimestamp = errors.New("found numbers that did not match a known timestamp format")
+)
 
 func parse(z zoner, args []string) (outputZones []*time.Location, t time.Time, inputZone *time.Location, err error) {
+	t = nullTime
+	inputZone = z.Zone()
+	var (
+		tmpZone *time.Location
+	)
 	if len(args) == 0 {
-		return nil, nullTime, nil, ErrNoArgs
+		return outputZones, t, inputZone, ErrNoArgs
 	}
 
-	var (
-		zoneRX = regexp.MustCompile(`^[a-zA-Z+\-]`)
-		fragRX = regexp.MustCompile("^[0-9]")
-		// outputZoneStrings are timezone strings positioned before any timestamps
-		outputZoneStrings []string
-		// outputZones       []*time.Location
-		// frags are number-containing strings that should be fragments of a timestamp
-		frags []string
-		//inputZoneString are timezone strings positioned after any timestamps
-		inputZoneString string
-		// inputZone       *time.Location
-		tmpZone    *time.Location
-		errTZParse error
-		errTSParse error
-	)
+	outputZoneStrings, timeFrags, inputZoneString := parseToStrings(args)
 
+	if inputZoneString != "" {
+		inputZone, err = parseZone(inputZoneString)
+		if err != nil {
+			return outputZones, t, inputZone, err
+		}
+	}
+
+	for _, tzString := range outputZoneStrings {
+		tmpZone, err = parseZone(tzString)
+		if err != nil {
+			return outputZones, t, inputZone, err
+		}
+		outputZones = append(outputZones, tmpZone)
+	}
+
+	if len(timeFrags) > 0 {
+		timeString := strings.Join(timeFrags, " ")
+		for _, f := range tsFormats {
+			t, err = time.ParseInLocation(f, timeString, inputZone)
+			if err == nil {
+				break
+			}
+		}
+	}
+
+	return outputZones, t, inputZone, err
+}
+
+func parseToStrings(args []string) (outputZoneStrings []string, timeFrags []string, inputZoneString string) {
 	for _, arg := range args {
 		if arg == "at" || arg == "in" || arg == "from" || arg == "to" {
 			continue
 		}
 		isFrag := fragRX.MatchString(arg)
 		if isFrag {
-			frags = append(frags, arg)
+			timeFrags = append(timeFrags, arg)
 			continue
 		}
 		isZone := zoneRX.MatchString(arg)
 		if isZone {
-			if len(frags) == 0 {
+			if len(timeFrags) == 0 {
 				outputZoneStrings = append(outputZoneStrings, arg)
 				continue
 			}
@@ -66,53 +89,18 @@ func parse(z zoner, args []string) (outputZones []*time.Location, t time.Time, i
 			}
 		}
 	}
-	/*
-		fmt.Println("Parse results:")
-		fmt.Println(outputZoneStrings)
-		fmt.Println(frags)
-		fmt.Println(inputZone)
-	*/
-	if inputZoneString != "" {
-		inputZone, errTZParse = time.LoadLocation(inputZoneString)
-		if errTZParse != nil {
-			err = errTZParse
-			return outputZones, t, inputZone, err
-		}
-	}
-	for _, tzString := range outputZoneStrings {
-		tmpZone, errTZParse = time.LoadLocation(tzString)
-		if errTZParse != nil {
-			// retry with alias:
-			alias, ok := tzAlias[tzString]
-			if !ok {
-				return nil, nullTime, nil, fmt.Errorf("could not parse %s as timezone", tzString)
-			}
-			tmpZone, errTZParse = time.LoadLocation(alias)
-			if errTZParse != nil {
-				return nil, nullTime, nil, fmt.Errorf("could not parse %s as timezone", tzString)
-			}
-		}
-		// successfully parsed tzString into tz
-		outputZones = append(outputZones, tmpZone)
-	}
+	return outputZoneStrings, timeFrags, inputZoneString
+}
 
-	t = nullTime
-	if len(frags) > 0 {
-		timeString := strings.Join(frags, " ")
-		for i, f := range tsFormats {
-			if inputZone != nil {
-				t, errTSParse = time.ParseInLocation(f, timeString, inputZone)
-			} else {
-				t, errTSParse = time.ParseInLocation(f, timeString, z.Zone())
-			}
-			if errTSParse == nil {
-				break
-			}
-			if i == len(tsFormats)-1 {
-				err = errParsingTimestamp
-			}
+func parseZone(s string) (*time.Location, error) {
+	tmp, err := time.LoadLocation(s)
+	if err != nil {
+		// retry with alias:
+		alias, ok := tzAlias[s]
+		if !ok {
+			return nil, fmt.Errorf("could not parse %s as timezone", s)
 		}
+		tmp, err = time.LoadLocation(alias)
 	}
-
-	return outputZones, t, nil, err
+	return tmp, err
 }
