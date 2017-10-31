711ad38f0079cb9c3f8d720cff661295af2c32de
therealplato <therealplato@gmail.com>
2017-10-31 21:18:46 +1300

    combine alias, formats > defaults

--- defaults.go
+++ defaults.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 var defaultAlias = map[string]string{
 	"ET":       "US/Eastern",
 	"et":       "US/Eastern",
@@ -17,4 +19,17 @@ var defaultAlias = map[string]string{
 	"auckland": "Pacific/Auckland",
 	"London":   "Europe/London",
 	"london":   "Europe/London",
+}
+
+var tsFormats = []string{
+	"2006-01-02 15:04:05",
+	"2006-01-02 15:04:05 -0700",
+	time.RFC1123,
+	time.RFC1123Z,
+	time.RFC3339,
+	time.RFC3339Nano,
+	time.RFC822,
+	time.RFC822Z,
+	time.RFC850,
+	time.ANSIC,
 }
